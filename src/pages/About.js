73c7c8a3fffd675654a1898ae6bc5ee0f885cc93
skyejackson <78817282+skyejackson@users.noreply.github.com>
2022-06-12 14:11:04 -0400

    updated name and description

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,8 +4,27 @@ export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+      <div class="split left">
+        <div className="centered">
+          <img 
+            className="profile_image"
+            // Image goes here
+            alt="Profile Pic"
+            ></img>
+        </div>
       </div>
+      <div className="split right">
+        <div className="centered">
+          <div className="name_title">Skye Jackson</div>
+          <div className="brief_description">
+          I am a rising junior computer science major at Spelman College in Atlanta, GA.
+          I love reading, traveling, coding, and learning about new cultures. I am so excited
+          to participate in the IBM Accelerate Program this Summer, and I am looking forward to
+          networking and improving both my technical and professional skills.
+          </div>
+        </div>
+      </div>
+    </div>  
     )
   }
 }
